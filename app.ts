cd49c4d87ce0568cffe3c8e57cdf8c4c3463cc63
Black Cat <rborole06@gmail.com>
2025-08-08 14:47:18 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-test
+import express, { Application } from "express";
+import bookRoutes from "./routes/bookRoutes";
+
+const app: Application = express();
+app.use(express.json());
+
+app.use("/api/books", bookRoutes);
+
+export default app;
